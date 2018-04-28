c02c86b160d707bca52379c0bbb6b35eb4588bc3
quan.zhou 周权 <z31125@hytera.com>
2018-04-28 17:30:25 +0800

    Added a card, edited a loooooooot of js

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -13,15 +13,22 @@
     }
   }
 
-  FormHandler.prototype.addSubmitHandler = function () {
+  FormHandler.prototype.addSubmitHandler = function (fn) {
     console.log("Setting submit handler for form");
     this.$formElement.on("submit", function(event) {
       event.preventDefault();
 
-      var data = $(this).serialzeArray();
+      var data = {};
+      $(this).serializeArray().forEach(function (item){
+        data[item.name] = item.value;
+        console.log(item.name + " is " + item.value);
+      });
       console.log(data);
-    })
-  }
+      fn(data);
+      this.reset();
+      this.elements[0].focus();
+    });
+  };
 
   App.FormHandler = FormHandler;
   window.App = App;
